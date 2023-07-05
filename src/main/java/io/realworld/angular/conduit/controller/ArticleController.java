b4239697d9944d78686cf58a1ca2a2037863c25f
boburmirzaqulov <ab200mirzaqulov@gmail.com>
2023-07-05 16:54:11 +0500

    ArticleController

---
+++ src/main/java/io/realworld/angular/conduit/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package io.realworld.angular.conduit.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/articles")
+public class ArticleController {
+}
