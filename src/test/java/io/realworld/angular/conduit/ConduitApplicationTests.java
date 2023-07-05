5e255c7a700e04153db090c37c11bd6ed47740a0
boburmirzaqulov <ab200mirzaqulov@gmail.com>
2023-07-05 16:45:12 +0500

    Initial commit

---
+++ src/test/java/io/realworld/angular/conduit/ConduitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.realworld.angular.conduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
